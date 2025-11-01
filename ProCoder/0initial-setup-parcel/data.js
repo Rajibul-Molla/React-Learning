a4508a388c1aff415c0aa18ce675df997fefc181
Rajibul Molla <rajibulat@gmail.com>
2025-11-02 02:23:55 +0530

    Learning Parcel Initial SetUp

---
+++ ProCoder/0initial-setup-parcel/data.js
@@ -0,0 +1,6 @@
+// export default {
+//   name: "Rajibul Molla",
+// }
+export const user= {
+  name: "Rajibul Molla",
+};
---
+++ ProCoder/0initial-setup-parcel/script.js
@@ -0,0 +1,12 @@
+// import {user} from "./data.js"
+// console.log(user)
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+// console.log(React)
+
+const x= <h1>From script.js</h1>
+
+const root=ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(x)
